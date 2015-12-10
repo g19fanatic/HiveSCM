ce46895456e2000195f2ede195a00581c719bc20
Paul Di Biase <paul.a.dibiase@gmail.com>
2015-12-09 22:04:24 -0500

    added front-end structure
    removed pyinstaller for now
    added a bat file that will run the python virt env and then run the app

---
+++ run_app.bat
@@ -0,0 +1,27 @@
+@echo off
+set "VIRTUAL_ENV=%~dp0\virt"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+	set "_OLD_VIRTUAL_PROMPT=%PROMPT%"	
+)
+set "PROMPT=(virt) %PROMPT%"
+
+if not defined _OLD_VIRTUAL_PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+)
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+python app.py
+:END
