55dfbf9717a88743c7b070d88047181d1d58e185
Paul Di Biase <paul.a.dibiase@gmail.com>
2016-03-18 14:56:55 -0400

    added nocache decorator
    also sorted tickets by id, hopefully latest first

--- app.py
+++ app.py
@@ -6,12 +6,26 @@ from werkzeug.debug import DebuggedApplication
 
 from os import listdir
 from os.path import isfile, join
+from functools import wraps, update_wrapper
+from datetime import datetime
 import json
 
 import pdb
 
 app = Flask(__name__, static_folder=None)
 app.debug = True
+
+#defines a nocache decorator for json endpoints
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+    return update_wrapper(no_cache, view)
 
 #forward static js files to ng frontend
 @app.route('/js/<path:path>')
@@ -45,14 +59,17 @@ def returnToIndex(p):
     return index()
 
 @app.route('/api/config')
+@nocache
 def getConfiguration():
     return returnFile('config.json')
 
 @app.route('/api/users')
+@nocache
 def getUsers():
     return returnFile('users.json')
 
 @app.route('/api/labels')
+@nocache
 def getLabels():
     return returnFile('labels.json')
 
@@ -65,6 +82,7 @@ def getTicketIds():
     return ticketIds
 
 @app.route('/api/tickets')
+@nocache
 def getTicketList():
     #pdb.set_trace()
     ticketFiles = listdir("data/tickets")
@@ -74,12 +92,14 @@ def getTicketList():
     for t in ticketFiles:
         with open("data/tickets/" + t) as f:
             ticket = json.loads(f.read())
+            ticket['id'] = int(ticket['id'])
             ticketsInfo.append(ticket)
 
     tickets['tickets'] = ticketsInfo
     return json.dumps(tickets)
 
 @app.route('/api/ticket/<int:ticketId>')
+@nocache
 def getTicketInfo(ticketId):
     tickets = getTicketIds()
     ticket = {}
@@ -91,6 +111,7 @@ def getTicketInfo(ticketId):
     return json.dumps(ticket)
 
 @app.route('/api/createTicket', methods=['POST'])
+@nocache
 def createTicket():
     ticketIds = getTicketIds()
     newTicket = json.loads(request.data)['ticket']
